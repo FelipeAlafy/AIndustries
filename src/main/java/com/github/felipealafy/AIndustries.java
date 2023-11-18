b737eb1e7048cdb59e840cc4418fdac8842b8f07
Felipe Alafy <69154350+FelipeAlafy@users.noreply.github.com>
2023-11-18 15:50:52 -0300

    First Items

--- src/main/java/com/github/felipealafy/AIndustries.java
+++ src/main/java/com/github/felipealafy/AIndustries.java
@@ -1,5 +1,6 @@
 package com.github.felipealafy;
 
+import com.github.felipealafy.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ public class AIndustries implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("AIndustries loading");
+		ModItems.RegisterModItems();
 	}
 }
---
+++ src/main/java/com/github/felipealafy/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.github.felipealafy.item;
+
+import com.github.felipealafy.AIndustries;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
+    public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+        entries.add(RAW_RUBY);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(AIndustries.MOD_ID, name), item);
+    }
+
+    public static void RegisterModItems() {
+        AIndustries.LOGGER.info("Registering Mod items fot " + AIndustries.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
+    }
+}
